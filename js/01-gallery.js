a1f5d1823e5271a8226e1325fbfae1c1b0d6c12d
Tetiana Kharlamova <tatianakh0301@ukr.net>
2022-08-23 18:01:38 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallaryList = document.querySelector('.gallery');
 
+const createGalaryCardMarkup = ({ preview, original, description }) => {
+    return `
+        <div class="gallery__item">
+            <a class="gallery__link" href=${preview}>
+                <img class="gallery__image" src=${preview} data-source=${original} alt=${description}/>
+            </a>
+        </div>
+        ` 
+    };
+    
+
+console.log(createGalaryCardMarkup(galleryItems[0]));
 console.log(galleryItems);
