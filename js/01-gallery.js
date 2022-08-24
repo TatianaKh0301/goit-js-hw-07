0f07f28d8de9d5d6c5bfbf096c01c0f59244d74b
Tetiana Kharlamova <tatianakh0301@ukr.net>
2022-08-24 15:09:44 +0300

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,19 +1,39 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
-const gallaryList = document.querySelector('.gallery');
+// const basicLightbox = require('basiclightbox');
+// import * as basicLightbox from 'basiclightbox';
 
-const createGalaryCardMarkup = ({ preview, original, description }) => {
+import { galleryItems } from './gallery-items.js';
+
+// Change code below this line
+const galleryList = document.querySelector('.gallery');
+
+galleryList.addEventListener('click', onGalleryItemclick);
+
+const createGalleryCardMarkup = ({ preview, original, description }) => {
     return `
         <div class="gallery__item">
-            <a class="gallery__link" href=${preview}>
+            <a class="gallery__link" href=${original}>
                 <img class="gallery__image" src=${preview} data-source=${original} alt=${description}/>
             </a>
         </div>
         ` 
     };
     
-const createGalaryCards = galleryItems.map(createGalaryCardMarkup).join(' ');
-gallaryList.insertAdjacentHTML('beforeend', createGalaryCards);
+const createGalleryCards = galleryItems.map(createGalleryCardMarkup).join(' ');
+galleryList.insertAdjacentHTML('beforeend', createGalleryCards);
 
+function onGalleryItemclick(event) {
+    event.preventFefault();
+    if (!event.target.classList.contains('gallery__image"')) {
+        return; 
+    } else {
+        console.log("event.target.dataset.source", event.target.dataset.source);
+
+        // const selectedImageUrl = event.target.dataset.source;
+        // const instance = basicLightbox.create(`
+        // <img src=${selectedImageUrl} width="1280" height="600">
+        // `);
+        // instance.show();        
+    }
+}
 
 console.log(galleryItems);
